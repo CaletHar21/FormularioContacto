88a856cad6a912124016981329fc57e83f8ea91b
PC <PC@MV2>
2024-02-28 19:25:18 -0600

    Agregar archivos de proyecto.

---
+++ Pages/contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FormularioContacto.Pages
+{
+    public class contactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
